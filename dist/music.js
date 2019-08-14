ec2815703d27bc883eda8800fa788b2d066db1d1
Sir-kay <jianghaokai.kaikai@qq.com>
2019-08-14 14:47:53 +0800

    Site updated: 2019-08-14 14:47:49

---
+++ dist/music.js
@@ -0,0 +1,55 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "Faded",
+        artist: 'Alan Walker',
+        url: 'http://fs.ios.kugou.com/201908141416/161e92dc6abeb7e0367c03a078854b38/G051/M01/03/05/04YBAFaDc0SAdBLDADQTZJXoo2Q475.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/240/20151231/20151231230206139997.jpg',
+      },
+      {
+        name: "All falls down",
+        artist: 'Alan Walker',
+        url: 'http://fs.ios.kugou.com/201908141419/c279ddec664bad998557932ce822d8e8/G142/M01/00/03/zg0DAFuMG6qAD1bNADEdO-2jvNA319.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/240/20171026/20171026225630480031.jpg',
+      },
+      {
+        name: "Alone",
+        artist: 'Alan Walker',
+        url: 'http://fs.ios.kugou.com/201908141429/4cc8842102ccf6692f630194260f402d/G076/M05/11/14/7IYBAFhLYpqAaRSOACafhOubKH8357.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/240/20161201/20161201225413538833.jpg',
+      },
+      {
+        name: "Ignite",
+        artist: 'Alan Walker',
+        url: 'http://fs.ios.kugou.com/201908141433/4f00d5a43803900d8171931854805e83/G131/M04/05/13/I4cBAFrz6wiAE1QRADNceNDT74g446.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/240/20180510/20180510144502608205.jpg',
+      },
+      {
+        name: "Routine",
+        artist: 'Alan Walker',
+        url: 'http://fs.ios.kugou.com/201908141443/9a8fc24266190909fc78200036afea6f/G080/M04/19/19/kA0DAFhb-QeABSszACjBiXgdY_U020.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/240/20161223/20161223000536443726.jpg',
+      },
+      {
+        name: "On My Way",
+        artist: 'Alan Walker',
+        url: 'http://fs.ios.kugou.com/201908141444/deee42abea1e5ed15abeec7a91324576/G157/M06/18/1B/PYcBAFyR4a2AFqypAC9M4KTY7Ok773.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/240/20190320/20190320144202248506.jpg',
+      },
+      {
+        name: "Move Your Body",
+        artist: 'Sia, Alan Walker',
+        url: 'http://fs.ios.kugou.com/201908141445/5cd502679dca26be6d096727a5dc55d8/G074/M08/01/1F/6oYBAFgJVlGAaKMhADU360IuMXk844.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/240/20161024/20161024113232837249.jpg',
+      },
+      {
+        name: "Sky",
+        artist: 'Sia, Alan Walker',
+        url: 'http://fs.ios.kugou.com/201908141446/34205d49efb8d00c0c629c0f14be4f3e/G108/M00/1A/11/rA0DAFk_EnqAFI2KAHwwxlxhQl8924.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/240/20170613/20170613062023344138.jpg',
+      }
+    ]
+});
