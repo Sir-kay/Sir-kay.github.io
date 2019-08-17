21fca55cef8957f282cc79fbdc234fce496167bd
Sir-kay <jianghaokai.kaikai@qq.com>
2019-08-17 12:39:34 +0800

    Site updated: 2019-08-17 12:39:31

--- dist/music.js
+++ dist/music.js
@@ -6,67 +6,61 @@ const ap = new APlayer({
       {
         name: "Faded",
         artist: 'Alan Walker',
-        url: 'http://fs.open.kugou.com/d99457d0a76a63d36eb8ccb83761f894/5d551a7c/G051/M01/03/05/04YBAFaDc0SAdBLDADQTZJXoo2Q475.mp3',
+        url: 'http://www.ytmp3.cn/down/57068.mp3',
         cover: 'http://imge.kugou.com/stdmusic/240/20151231/20151231230206139997.jpg',
       },
       {
         name: "All falls down",
         artist: 'Alan Walker',
-        url: 'http://fs.open.kugou.com/c2e742bc75a9753f02d6e6da2083be20/5d55185c/G142/M01/00/03/zg0DAFuMG6qAD1bNADEdO-2jvNA319.mp3',
+        url: 'http://www.ytmp3.cn/down/48727.mp3',
         cover: 'http://imge.kugou.com/stdmusic/240/20171026/20171026225630480031.jpg',
       },
       {
         name: "Alone",
         artist: 'Alan Walker',
-        url: 'http://fs.open.kugou.com/668160e79bd57418c97795b883f604b1/5d551ad1/G076/M05/11/14/7IYBAFhLYpqAaRSOACafhOubKH8357.mp3',
+        url: 'http://www.ytmp3.cn/down/50557.mp3'
         cover: 'http://imge.kugou.com/stdmusic/240/20161201/20161201225413538833.jpg',
       },
       {
         name: "Ignite",
         artist: 'K-391,Alan Walker',
-        url: 'http://fs.open.kugou.com/ec8532cd381fcca0aff92713a218bc43/5d551b0c/G131/M04/05/13/I4cBAFrz6wiAE1QRADNceNDT74g446.mp3',
+        url: 'http://www.ytmp3.cn/down/57700.mp3',
         cover: 'http://imge.kugou.com/stdmusic/240/20180510/20180510144502608205.jpg',
       },
       {
         name: "Routine",
         artist: 'Alan Walker',
-        url: 'http://fs.open.kugou.com/a3bb5f9931248cd4cd05919e9ddd7c95/5d551b2a/G080/M04/19/19/kA0DAFhb-QeABSszACjBiXgdY_U020.mp3',
+        url: 'http://www.ytmp3.cn/down/36059.mp3',
         cover: 'http://imge.kugou.com/stdmusic/240/20161223/20161223000536443726.jpg',
       },
       {
         name: "On My Way",
         artist: 'Alan Walker',
-        url: 'http://fs.open.kugou.com/934e5fcd99dd9bd5c06f1d2c01f1f0d4/5d5519d3/G157/M06/18/1B/PYcBAFyR4a2AFqypAC9M4KTY7Ok773.mp3',
+        url: 'http://www.ytmp3.cn/down/60024.mp3',
         cover: 'http://imge.kugou.com/stdmusic/240/20190320/20190320144202248506.jpg',
-      },
-      {
-        name: "Sky",
-        artist: 'Alan Walker',
-        url: 'http://fs.open.kugou.com/3c156a90e1bf020fe3ce6b972a6c7e97/5d551bb7/G106/M0B/07/19/SpQEAFk_EfWAMzziADGtrHYCeNc616.mp3',
-        cover: 'http://imge.kugou.com/stdmusic/240/20170613/20170613062023344138.jpg',
       },
       {
         name: "Darkside",
         artist: 'Alan Walker',
-        url: 'http://fs.open.kugou.com/351250a86654fd883806e701142956fd/5d551bce/G133/M00/09/03/xQ0DAFtZ6huAIAVVADO-IdwEQgo093.mp3',
+        url: 'http://www.ytmp3.cn/down/51367.mp3',
         cover: 'http://imge.kugou.com/stdmusic/240/20180726/20180726223034633026.jpg',
       },
       {
         name: "Diamond Heart",
         artist: 'Alan Walker',
-        url: 'http://fs.open.kugou.com/a3e4fff11efe4f68dc31b50bc424c9f4/5d551b81/G093/M01/04/1F/PZQEAFusjwOAY5YXADqSvgae77s410.mp3',
+        url: 'http://www.ytmp3.cn/down/57069.mp3',
         cover: 'http://imge.kugou.com/stdmusic/240/20180927/20180927160215604448.jpg',
       },
       {
         name: "Spectre",
         artist: 'Alan Walker',
-        url: 'http://fs.open.kugou.com/a3e4fff11efe4f68dc31b50bc424c9f4/5d551b81/G093/M01/04/1F/PZQEAFusjwOAY5YXADqSvgae77s410.mp3',
+        url: 'http://www.ytmp3.cn/down/36060.mp3',
         cover: 'http://imge.kugou.com/stdmusic/240/20150720/20150720143238297086.jpg',
       },
       {
         name: "The Spectre",
         artist: 'Alan Walker',
-        url: 'http://fs.open.kugou.com/a12f8083901ff388d963b9001207332e/5d551c2a/G111/M00/0D/11/rw0DAFm6mGSABCe6AC9TMn7VzHk537.mp3',
+        url: 'http://www.ytmp3.cn/down/43954.mp3',
         cover: 'http://imge.kugou.com/stdmusic/240/20170914/20170914225307625638.jpg',
       }
     ]
